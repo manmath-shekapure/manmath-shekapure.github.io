c284399cc9225a2bee30896666402d35329a10d0
manmath <manmathshekapure7@gmail.com>
2026-02-14 10:54:59 +0530

    Updated portfolio with new logic

--- project/E-commerce/app.js
+++ project/E-commerce/app.js
@@ -1,216 +1,49 @@
 
-// const categoryList = document.getElementById("categoryList");
-// const productList = document.getElementById("productList");
-// const pagination = document.getElementById("pagination");
-// const searchInput = document.getElementById("searchInput");
-// const searchBtn = document.getElementById("searchBtn");
-
-
-// let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-
-// let allproducts = [];
-// let filterproducts = [];
-// let currentpage = 1;
-// let limit = 10;
-
-// /* Load all products */
-// fetch("https://dummyjson.com/products?limit=100")
-//   .then(res => res.json())
-//   .then(data => {
-//     allproducts = data.products;
-//     filterproducts = allproducts;
-//     render();
-//   });
-
-// /* SEARCH FUNCTION */
-// function applySearch() {
-//   const keyword = searchInput.value.toLowerCase();
-
-//   filterproducts = allproducts.filter(p =>
-//     p.title.toLowerCase().includes(keyword)
-//   );
-
-//   currentpage = 1;
-//   render();
-// }
-
-// searchInput.addEventListener("input", applySearch);
-// searchBtn.addEventListener("click", applySearch);
-
-// /* LOAD CATEGORY LIST */
-// fetch("https://dummyjson.com/products/category-list")
-//   .then(res => res.json())
-//   .then(categories => {
-//     categoryList.innerHTML = "";
-
-//     categories.forEach(category => {
-//       categoryList.innerHTML += `
-//         <li>
-//           <a class="dropdown-item" href="#" onclick="loadProduct('${category}')">${category}</a>
-//         </li>`;
-//     });
-//   });
-
-// /* CATEGORY WISE LOAD */
-// function loadProduct(cat) {
-//   fetch(`https://dummyjson.com/products/category/${cat}`)
-//     .then(res => res.json())
-//     .then(data => {
-//       allproducts = data.products;
-//       filterproducts = allproducts;
-//       currentpage = 1;
-//       render();
-//     });
-// }
-
-// /* DISPLAY PRODUCTS */
-// function displayProducts() {
-//   productList.innerHTML = "";
-
-//   let start = (currentpage - 1) * limit;
-//   let end = start + limit;
-
-//   const items = filterproducts.slice(start, end);
-
-//   items.forEach(product => {
-//     productList.innerHTML += `
-//       <div class="col-md-3 mb-4">
-//         <div class="pro-card">
-//           <img src="${product.thumbnail}" class="pro-img">
-//           <h5 class="pro-title">${product.title}</h5>
-//           <div class="price-box">
-//             <span class="new-price">₹${product.price}</span>
-//             <button class='btn-sm  btn-primary'
-//             onclick ='addTocart(${json.stringify(product)})'Add to cart</button>
-//           </div>
-//           </div>
-//       </div>`;
-//   });
-// }
-
-// /* PAGINATION */
-// function setupPagination() {
-//   pagination.innerHTML = "";
-
-//   const pages = Math.ceil(filterproducts.length / limit);
-
-//   for (let i = 1; i <= pages; i++) {
-//     pagination.innerHTML += `
-//       <li class="page-item ${i === currentpage ? "active" : ""}">
-//         <a class="page-link" href="#" onclick="changepage(${i})">${i}</a>
-//       </li>`;
-//   }
-// }
-
-// function changepage(p) {
-//   currentpage = p;
-//   render();
-// }
-
-// /* MAIN RENDER */
-// function render() {
-//   displayProducts();
-//   setupPagination();
-// }
-
-
-
-// function displayProducts() {
-//   productList.innerHTML = "";
-
-//   let start = (currentpage - 1) * limit;
-//   let end = start + limit;
-
-//   const items = filterproducts.slice(start, end);
-
-//   items.forEach(product => {
-//     productList.innerHTML += `
-//       <div class="col-md-3 mb-4">
-//         <div class="pro-card">
-//           <img src="${product.thumbnail}" class="pro-img">
-//           <h5 class="pro-title">${product.title}</h5>
-
-//           <div class="price-box">
-//             <span class="new-price">₹${product.price}</span>
-//           </div>
-
-//           <button class="btn btn-primary w-100 mt-2" onclick="addToCart(${product.id})">
-//                 Add to Cart
-//                  </button>
-
-//         </div>
-//       </div>`;
-//   });
-// }
-
-
-// function addToCart(id) {
-
-//     const product = allproducts.find(p => p.id === id);
-
-//     const item = cart.find(c => c.id === id);
-
-//     if (item) {
-//         item.qty++;
-//     } else {
-//         cart.push({
-//             id: product.id,
-//             title: product.title,
-//             price: product.price,
-//             qty: 1
-//         });
-//     }
-
-//     saveCart();
-//     renderCart();
-// }
-
-//   function savecart(){
-//     localStorage.setItem("cart",json.stringify(cart));
-//     rendercart();
-//   }
-
-
-//   function rendercart(){
-//   const cartBody =document.getElementById("cartBody");
-//   const cartTotal = document.getElementById("cartTotal");
-//   cartBody.innerHTML = "";
-//   let total  = 0;
-// }
-
-// function saveCart() {
-//     localStorage.setItem("cart", JSON.stringify(cart));
-// }
-
-// ELEMENTS
 const categoryList = document.getElementById("categoryList");
 const productList = document.getElementById("productList");
 const pagination = document.getElementById("pagination");
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
+const cartBody = document.getElementById("cartBody");
+const cartTotal = document.getElementById("cartTotal");
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-let allproducts = [];
-let filterproducts = [];
+let allproducts = [];       
+let currentViewProducts = []; 
 let currentpage = 1;
 let limit = 10;
 
-/* Load all products */
+
+
+let selectedCategory = localStorage.getItem("selectedCategory");
+
+if (selectedCategory) {
+    fetch(`https://dummyjson.com/products/category/${selectedCategory}`)
+      .then(res => res.json())
+      .then(data => {
+          currentViewProducts = data.products;
+          currentpage = 1;
+          render();
+      });
+
+    localStorage.removeItem("selectedCategory");
+}
+
 fetch("https://dummyjson.com/products?limit=100")
   .then(res => res.json())
   .then(data => {
     allproducts = data.products;
-    filterproducts = allproducts;
+    currentViewProducts = [...allproducts];  
     render();
   });
 
-/* SEARCH FUNCTION */
+
+
 function applySearch() {
   const keyword = searchInput.value.toLowerCase();
 
-  filterproducts = allproducts.filter(p =>
+  currentViewProducts = allproducts.filter(p =>
     p.title.toLowerCase().includes(keyword)
   );
 
@@ -221,7 +54,8 @@ function applySearch() {
 searchInput.addEventListener("input", applySearch);
 searchBtn.addEventListener("click", applySearch);
 
-/* LOAD CATEGORY LIST */
+
+
 fetch("https://dummyjson.com/products/category-list")
   .then(res => res.json())
   .then(categories => {
@@ -235,26 +69,25 @@ fetch("https://dummyjson.com/products/category-list")
     });
   });
 
-/* CATEGORY WISE LOAD */
+
+
 function loadProduct(cat) {
-  fetch(`https://dummyjson.com/products/category/${cat}`)
-    .then(res => res.json())
-    .then(data => {
-      allproducts = data.products;
-      filterproducts = allproducts;
-      currentpage = 1;
-      render();
-    });
+
+  currentViewProducts = allproducts.filter(p => p.category === cat);
+
+  currentpage = 1;
+  render();
 }
 
-/* DISPLAY PRODUCTS */
+
+
 function displayProducts() {
   productList.innerHTML = "";
 
   let start = (currentpage - 1) * limit;
   let end = start + limit;
 
-  const items = filterproducts.slice(start, end);
+  const items = currentViewProducts.slice(start, end);
 
   items.forEach(product => {
     productList.innerHTML += `
@@ -267,21 +100,20 @@ function displayProducts() {
             <span class="new-price">₹${product.price}</span>
           </div>
 
-          <button class="btn btn-primary w-100 mt-2" onclick='addToCart(${JSON.stringify(product)})'>
-    Add to Cart
-</button>
-
-
+          <button class="btn btn-primary w-100 mt-2" onclick="addToCart(${product.id})">
+              Add to Cart
+          </button>
         </div>
       </div>`;
   });
 }
 
-/* PAGINATION */
+
+
 function setupPagination() {
   pagination.innerHTML = "";
 
-  const pages = Math.ceil(filterproducts.length / limit);
+  const pages = Math.ceil(currentViewProducts.length / limit);
 
   for (let i = 1; i <= pages; i++) {
     pagination.innerHTML += `
@@ -296,52 +128,114 @@ function changepage(p) {
   render();
 }
 
-/* MAIN RENDER */
+
+
 function render() {
   displayProducts();
   setupPagination();
 }
 
-/* ADD TO CART */
+
+// FIXED — ADD TO CART
 function addToCart(id) {
 
   const product = allproducts.find(p => p.id === id);
-  if (!product) return;
 
   const item = cart.find(c => c.id === id);
 
   if (item) {
-      item.qty++;
+    item.qty++;
   } else {
-      cart.push({
-          id: product.id,
-          title: product.title,
-          price: product.price,
-          qty: 1
-      });
+    cart.push({
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      qty: 1
+    });
   }
 
   saveCart();
+  renderCart();
 }
 
-// /* SAVE CART */
-// function saveCart() {
-//   localStorage.setItem("cart", JSON.stringify(cart));
-// }
-// function addToCart(product) {
 
-//     const item = cart.find(c => c.id === product.id);
 
-//     if (item) {
-//         item.qty++;
-//     } else {
-//         cart.push({
-//             id: product.id,
-//             title: product.title,
-//             price: product.price,
-//             qty: 1
-//         });
-//     }
+function saveCart() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
-//     localStorage.setItem("cart", JSON.stringify(cart));
-// }
+
+function renderCart() {
+  cartBody.innerHTML = "";
+  let total = 0;
+
+  cart.forEach(item => {
+    const itemTotal = item.price * item.qty;
+    total += itemTotal;
+
+    cartBody.innerHTML += `
+      <tr>
+        <td>${item.title}</td>
+
+        <td>
+          <button class="btn btn-sm btn-secondary" onclick="decreaseQty(${item.id})">−</button>
+          <span class="mx-2">${item.qty}</span>
+          <button class="btn btn-sm btn-secondary" onclick="increaseQty(${item.id})">+</button>
+        </td>
+
+        <td>₹${item.price}</td>
+        <td>₹${itemTotal}</td>
+
+        <td>
+          <button class="btn btn-danger btn-sm" onclick="removeItem(${item.id})">Remove</button>
+        </td>
+      </tr>`;
+  });
+
+  cartTotal.innerText = total;
+}
+
+
+
+function removeItem(id) {
+  cart = cart.filter(item => item.id !== id);
+  saveCart();
+  renderCart();
+}
+
+
+
+renderCart();
+
+function increaseQty(id) {
+  const item = cart.find(p => p.id === id);
+  if (item) {
+    item.qty++;
+  }
+  saveCart();
+  renderCart();
+}
+
+function decreaseQty(id) {
+  const item = cart.find(p => p.id === id);
+
+  if (item.qty > 1) {
+    item.qty--;
+  } else {
+    // If qty goes below 1 → remove product
+    cart = cart.filter(p => p.id !== id);
+  }
+
+  saveCart();
+  renderCart();
+}
+function removeItem(id) {
+  const item = cart.find(p => p.id === id);
+
+  if (confirm(`Are you sure you want to remove "${item.title}" from the cart?`)) {
+    cart = cart.filter(p => p.id !== id);
+    saveCart();
+    renderCart();
+    alert(`${item.title} removed successfully`);
+  }
+}
